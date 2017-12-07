53b38588aa73b1cbf3951f54d7db0f7cf60aa7b4
e <shenbaoone@gmail.com>
2017-12-07 22:47:07 +0800

    update

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Code404Component } from './code404/code404.component';
+import { IndexComponent } from './index/index.component';
+
+const routerConfig = [
+  {
+    path: '',
+    component: IndexComponent
+  },
+  {
+    path: '**',
+    component: Code404Component
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Code404Component,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routerConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
