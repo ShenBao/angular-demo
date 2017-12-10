411398342e63dbdfdba28ee0ddfaa8c72a8022de
e <shenbaoone@gmail.com>
2017-12-10 23:49:03 +0800

    update

--- client-angular/src/app/router/router002/router002.component.ts
+++ client-angular/src/app/router/router002/router002.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-router002',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Router002Component implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  toIndex() {
+    this.router.navigate(["/index"]);
+  }
 
   ngOnInit() {
   }
